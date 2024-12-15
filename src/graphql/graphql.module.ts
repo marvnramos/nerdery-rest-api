0037555a6aba2ed816292a5382cc6ec122701fc8
marvnramos <mr487223@gmail.com>
2024-12-15 12:11:37 -0600

    ✨ setting up graphql module

---
+++ src/graphql/graphql.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      playground: true,
+      debug: true,
+      context: ({ req }) => ({ request: req }),
+    }),
+  ],
+})
+export class GraphqlModule {}
