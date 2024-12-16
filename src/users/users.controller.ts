b882f38807baad39f9ffff8dd29c8e4b736545f6
marvnramos <marvinramos@ravn.co>
2024-12-16 14:12:53 -0600

    ✨ add email sender function to validate user credentials

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,26 @@
 import {
   BadRequestException,
   Body,
-  Controller,
+  Controller, Get,
   HttpCode,
   HttpStatus,
+  Param,
   Post,
+  Put,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { SignupReqDto } from './dto/requests/signup.req.dto';
 import { SignUpResDto } from './dto/responses/signup.res.dto';
+import { randomUUID } from 'crypto';
+import { MailService } from '../utils/mailer/mail.service';
+import { getExpirationTimestamp } from '../utils/timestamp';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly mailService: MailService,
+  ) {}
 
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
@@ -26,6 +34,27 @@ export class UsersController {
     const userData = { ...req, is_email_verified: false };
 
     const user = await this.usersService.create(userData);
+
+    const token = randomUUID();
+    await this.usersService.addVerificationToken({
+      token: token,
+      is_used: false,
+      expired_at: getExpirationTimestamp(),
+      user: {
+        connect: { id: user.id },
+      },
+      tokenType: {
+        connect: { id: 1 },
+      },
+    });
+
+    const encodedToken = await this.usersService.encodeVerificationToken(token);
+    await this.mailService.sendUserConfirmationEmail(
+      req.email,
+      `${user.first_name} ${user.last_name}`,
+      encodedToken,
+    );
+
     return {
       created_at: user.created_at,
     };
