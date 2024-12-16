21a901837b93197fa70e0d11b476993ee3739de1
marvnramos <mr487223@gmail.com>
2024-12-15 21:24:49 -0600

    ✨ add modules, controllers and providers into app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { StripeController } from './stripe/stripe.controller';
+import { StripeModule } from './stripe/stripe.module';
+import { GraphqlModule } from './graphql/graphql.module';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    StripeModule,
+    GraphqlModule,
+    UsersModule,
+    PrismaModule,
+  ],
+  controllers: [AuthController, StripeController, UsersController],
+  providers: [AuthService, PrismaService],
 })
 export class AppModule {}
