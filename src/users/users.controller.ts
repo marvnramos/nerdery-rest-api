5acacd253b56fd03e342a060d6106f035159f609
marvnramos <mr487223@gmail.com>
2024-12-15 21:27:04 -0600

    ✨ add sign up end point feature

---
+++ src/users/users.controller.ts
@@ -0,0 +1,33 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { SignupReqDto } from './dto/requests/signup.req.dto';
+import { SignUpResDto } from './dto/responses/signup.res.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  async signUp(@Body() req: SignupReqDto): Promise<SignUpResDto> {
+    const existingUser = await this.usersService.findByEmail(req.email);
+    if (existingUser) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    req.password = await this.usersService.hashPassword(req.password);
+    const userData = { ...req, is_email_verified: false };
+
+    const user = await this.usersService.create(userData);
+    return {
+      created_at: user.created_at,
+    };
+  }
+}
