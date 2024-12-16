411d42c6bd7bdd17e077cafe140947f940f49b6a
marvnramos <marvinramos@ravn.co>
2024-12-16 14:13:19 -0600

    ✨ add mail module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { MailModule } from '../utils/mailer/mail.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MailModule],
   providers: [UsersService],
   exports: [UsersService],
 })
