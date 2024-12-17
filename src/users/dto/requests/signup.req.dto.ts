7b24b3f5ed8456901911cf7f0b7108d991ecd636
marvnramos <marvinramos@ravn.co>
2024-12-17 13:12:30 -0600

    ✨ add defined property in class validator

--- src/users/dto/requests/signup.req.dto.ts
+++ src/users/dto/requests/signup.req.dto.ts
@@ -2,11 +2,13 @@ import { IsDefined, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { Expose } from 'class-transformer';
 
 export class SignupReqDto {
+  @IsDefined()
   @IsString()
   @IsNotEmpty()
   @Expose({ name: 'first_name' })
   first_name: string;
 
+  @IsDefined()
   @IsString()
   @IsNotEmpty()
   @Expose({ name: 'last_name' })
@@ -18,10 +20,12 @@ export class SignupReqDto {
   @IsEmail()
   email: string;
 
+  @IsDefined()
   @IsString()
   @IsNotEmpty()
   address: string;
 
+  @IsDefined()
   @IsString()
   @IsNotEmpty()
   password: string;
