0e63756c535ffb756199bd47b12b0bf98a33a7e7
marvnramos <marvinramos@ravn.co>
2024-12-18 09:52:29 -0600

    ✨ add login and logout controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local.guard';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+import { SignInResDto } from './dto/response/sign.in.res.dto';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { UsersService } from 'src/users/users.service';
+import { SignInReqDto } from './dto/request/sign.in.req.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService,
+  ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Body() req: SignInReqDto, @Res() res: Response): Promise<void> {
+    const user = await this.userService.findByEmail(req.email);
+    const response: SignInResDto = await this.authService.login(user);
+
+    res.cookie('access_token', response.accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 60 * 60 * 1000,
+      sameSite: 'strict',
+    });
+
+    res.json(response);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  async logout(@Res() res: Response): Promise<void> {
+    res.clearCookie('access_token');
+    res.json({ message: 'Logged out successfully' });
+  }
+}
