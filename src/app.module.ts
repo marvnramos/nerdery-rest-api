fa6a3d0ed881540f0a8ffead9910b777c288b891
marvnramos <marvinramos@ravn.co>
2024-12-18 14:39:06 -0600

    ✨ add stripe controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
-import { StripeController } from './stripe/stripe.controller';
-import { StripeModule } from './stripe/stripe.module';
+import { PaymentsController } from './payments/payments.controller';
+import { PaymentsModule } from './payments/payments.module';
 import { GraphqlModule } from './graphql/graphql.module';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
@@ -18,13 +18,13 @@ import { MailModule } from './utils/mailer/mail.module';
       isGlobal: true,
     }),
     AuthModule,
-    StripeModule,
+    PaymentsModule,
     GraphqlModule,
     UsersModule,
     PrismaModule,
     MailModule,
   ],
-  controllers: [AuthController, StripeController, UsersController],
+  controllers: [AuthController, PaymentsController, UsersController],
   providers: [PrismaService, MailService],
 })
 export class AppModule {}
