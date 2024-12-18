b875be07f4105609f1fd25182ac834597085bcef
marvnramos <marvinramos@ravn.co>
2024-12-18 14:37:56 -0600

    ✨ add payment module

---
+++ src/payments/payments.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PaymentsController } from './payments.controller';
+import { PaymentsService } from './payments.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [ConfigModule, PrismaModule],
+  controllers: [PaymentsController],
+  providers: [PaymentsService],
+})
+export class PaymentsModule {}
