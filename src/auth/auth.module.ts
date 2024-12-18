8f014e1e547648d51d9e64c159326f2240e9e224
marvnramos <mr487223@gmail.com>
2024-12-17 20:45:15 -0600

    ✨ add auth module setting

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthService } from './auth.service';
 
-@Module({})
+@Module({
+  imports: [PrismaModule],
+  providers: [AuthService],
+  exports: [AuthService],
+})
 export class AuthModule {}
