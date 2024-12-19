48e7130df7e19b13341ea71dbfd8c34865097333
marvnramos <marvinramos@ravn.co>
2024-12-19 13:27:20 -0600

    ✨ add user roles graphql model

---
+++ src/users/models/user.roles.model.ts
@@ -0,0 +1,20 @@
+import { Field, ObjectType, ID } from '@nestjs/graphql';
+import { User } from './users.model';
+
+@ObjectType()
+export class UserRoles {
+  @Field(() => ID)
+  id: string;
+
+  @Field()
+  role: string;
+
+  @Field({ name: 'created_at' })
+  createdAt: Date;
+
+  @Field({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @Field(() => [User], { nullable: true })
+  users?: User[];
+}
