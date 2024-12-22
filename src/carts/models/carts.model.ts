58be4c030f3ee6d9d5367886988861ed7e472cdb
marvnramos <mr487223@gmail.com>
2024-12-22 07:20:23 -0600

    ✨ add cart graphql model

--- src/carts/models/carts.model.ts
+++ src/carts/models/carts.model.ts
@@ -1,4 +1,20 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType, ID } from '@nestjs/graphql';
+import { CartItems } from './cart.items.model';
 
 @ObjectType()
-export class Cart {}
+export class Cart {
+  @Field(() => ID)
+  id: string;
+
+  @Field({ name: 'user_id' })
+  userId: string;
+
+  @Field({ name: 'created_at' })
+  createdAt: Date;
+
+  @Field({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @Field(() => [CartItems], { nullable: true })
+  cartItems?: CartItems[];
+}
