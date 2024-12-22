07586f72933b3181dd84aeb0d0ef5e3acce8c2fb
marvnramos <marvinramos@ravn.co>
2024-12-22 09:46:57 -0600

    ♻️ using a single service to send mails

--- src/utils/mailer/mail.service.ts
+++ src/utils/mailer/mail.service.ts
@@ -1,37 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
-import * as process from 'node:process';
+import { EmailCommand } from './dto/email.command';
 
 @Injectable()
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
 
-  async sendUserConfirmationEmail(
-    email: string,
-    fullName: string,
-    token: string,
-  ) {
-    const url = `${process.env.BASE_URL}/users/validate-email/${token}`;
+  async sendEmail(emailCommand: EmailCommand) {
     await this.mailerService.sendMail({
-      to: email,
-      subject: 'Confirm your email',
-      template: './confirmation',
+      to: emailCommand.email,
+      subject: emailCommand.subject,
+      template: emailCommand.template,
       context: {
-        name: fullName,
-        url,
-      },
-    });
-  }
-
-  async sendResetPasswordEmail(email: string, fullName: string, token: string) {
-    const url = `${process.env.BASE_URL}/users/reset-password/${token}`;
-    await this.mailerService.sendMail({
-      to: email,
-      subject: 'Reset your password',
-      template: './reset-password',
-      context: {
-        name: fullName,
-        url,
+        name: emailCommand.fullName,
+        url: emailCommand.uri,
       },
     });
   }
