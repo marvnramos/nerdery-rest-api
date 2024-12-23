beebc04dc86a477a466a8790bf2931032acbbf39
marvnramos <marvinramos@ravn.co>
2024-12-23 13:48:09 -0600

    ✨ add PrismaModule import

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { CategoriesResolver } from './categories.resolver';
+import { PrismaModule } from 'src/utils/prisma/prisma.module';
 
 @Module({
-  providers: [CategoriesService],
+  imports: [PrismaModule],
+  providers: [CategoriesService, CategoriesResolver],
+  exports: [CategoriesService],
 })
 export class CategoriesModule {}
