3ed6912c611e1764de4f8df7f9c6e1514dc3d242
marvnramos <marvinramos@ravn.co>
2024-12-23 07:23:23 -0600

    ✨ add favorite graphql model

---
+++ src/favorites/models/favorites.models.ts
@@ -0,0 +1,19 @@
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+
+ObjectType();
+export class Favorite {
+  @Field(() => ID)
+  id: string;
+
+  @Field({ name: 'user_id' })
+  userId: string;
+
+  @Field({ name: 'product_id' })
+  productId: string;
+
+  @Field({ name: 'created_at' })
+  createdAt: Date;
+
+  @Field({ name: 'updated_at' })
+  updatedAt: Date;
+}
