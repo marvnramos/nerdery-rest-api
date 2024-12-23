6d70a7c2012ad0132c4b1efce356ece092c5404a
marvnramos <marvinramos@ravn.co>
2024-12-23 17:44:41 -0600

    🚧 add addCategory resolver

--- src/categories/categories.resolver.ts
+++ src/categories/categories.resolver.ts
@@ -1,4 +1,45 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { Categories } from './models/categories.model';
+import { UseGuards } from '@nestjs/common';
+import { GqlJwtAuthGuard } from '../auth/guards/gql.jwt.guard';
+import { AddCategoryReq } from './dto/requests/create.category.req';
+import { CategoriesService } from './categories.service';
+import { Prisma } from '@prisma/client';
 
 @Resolver()
-export class CategoriesResolver {}
+export class CategoriesResolver {
+  constructor(private readonly categoriesService: CategoriesService) {}
+  @Mutation(() => Categories)
+  @UseGuards(GqlJwtAuthGuard)
+  /**
+   * TODO: Add Roles Guard
+   */
+  async addCategory(@Args('data') data: AddCategoryReq): Promise<Categories> {
+    const datas: Prisma.CategoryCreateInput = {
+      category_name: data.categoryName,
+    };
+    return await this.categoriesService.createCategory(datas);
+  }
+}
+
+// @ObjectType()
+// export class Categories {
+//   @Field(() => ID)
+//   id: string;
+
+//   @Field({ name: 'category_name' })
+//   categoryName: string;
+
+//   @Field({ name: 'created_at' })
+//   createdAt: Date;
+
+//   @Field({ name: 'updated_at' })
+//   updatedAt: Date;
+// }
+
+// (alias) type Category = {
+//     id: number;
+//     category_name: string;
+//     created_at: Date;
+//     updated_at: Date;
+// }
