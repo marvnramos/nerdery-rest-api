aea8d67b23b452a9f410319a2697135dcd8265ed
marvnramos <marvinramos@ravn.co>
2024-12-23 13:55:38 -0600

    ✨ add create product service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Prisma, Product } from '@prisma/client';
+import { PrismaService } from 'src/utils/prisma/prisma.service';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private readonly prismaServce: PrismaService) {}
+
+  async createProduct(data: Prisma.ProductCreateInput): Promise<Product> {
+    try {
+      return await this.prismaServce.product.create({ data });
+    } catch (error) {
+      console.error('Error creating product:', error);
+      throw new InternalServerErrorException('Failed to create product');
+    }
+  }
+}
