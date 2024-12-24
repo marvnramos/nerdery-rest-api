77939e6216900ea25415a91717d78868b5a4a26f
marvnramos <mr487223@gmail.com>
2024-12-24 08:09:11 -0600

    ✨ add categories resolvers

--- src/categories/categories.resolver.ts
+++ src/categories/categories.resolver.ts
@@ -1,21 +1,45 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { Categories } from './models/categories.model';
 import { AddCategoryReq } from './dto/requests/create.category.req';
 import { CategoriesService } from './categories.service';
 import { plainToInstance } from 'class-transformer';
 import { Auth } from '../auth/decorators/auth.role.decorator';
+import { DeleteCategoryReq } from './dto/requests/delete.category.req';
+import { DeleteCategoryRes } from './dto/responses/delete.category.res';
+import { AddCategoryRes } from './dto/responses/create.category.res';
 
 @Resolver()
 export class CategoriesResolver {
   constructor(private readonly categoriesService: CategoriesService) {}
-  @Mutation(() => Categories)
+
+  @Mutation(() => AddCategoryRes)
   @Auth('MANAGER')
-  async addCategory(@Args('data') data: AddCategoryReq): Promise<Categories> {
+  async addCategory(
+    @Args('data') data: AddCategoryReq,
+  ): Promise<AddCategoryRes> {
     const { categoryName } = data;
     const category = await this.categoriesService.createCategory({
       category_name: categoryName,
     });
+    return plainToInstance(AddCategoryRes, category);
+  }
 
-    return plainToInstance(Categories, category);
+  @Mutation(() => DeleteCategoryRes)
+  @Auth('MANAGER')
+  async deleteCategory(
+    @Args('data') data: DeleteCategoryReq,
+  ): Promise<DeleteCategoryRes> {
+    await this.categoriesService.removeCategory(data.id);
+
+    const response = new DeleteCategoryRes();
+    response.deletedAt = new Date();
+    return response;
+  }
+
+  @Query(() => [Categories])
+  @Auth('MANAGER')
+  async getCategories(): Promise<Categories[]> {
+    const categories = await this.categoriesService.getAllCategories();
+    return plainToInstance(Categories, categories);
   }
 }
