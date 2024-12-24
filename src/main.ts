dfe7278b0b3b0992e77c54339bd314e59c6c68e0
marvnramos <mr487223@gmail.com>
2024-12-24 09:33:36 -0600

    🥅 add global exception filter in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import helmet from 'helmet';
+import { GlobalExceptionFilter } from './utils/GlobalExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -28,7 +29,7 @@ async function bootstrap() {
       },
     }),
   );
-
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
