ce204331786b2ceeaf78d9ab301a061b13da2e02
marvnramos <mr487223@gmail.com>
2024-12-24 17:55:55 -0600

    ✨ add cloudinary module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsResolver } from './products.resolver';
 import { PrismaModule } from 'src/utils/prisma/prisma.module';
+import { ProductsController } from './products.controller';
+import { CloudinaryModule } from '../utils/cloudinary/cloudinary.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CloudinaryModule],
   providers: [ProductsService, ProductsResolver],
   exports: [ProductsService],
+  controllers: [ProductsController],
 })
 export class ProductsModule {}
