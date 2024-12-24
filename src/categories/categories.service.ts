cb30216fecce81ccff754c1117e0d977d4666dbc
marvnramos <mr487223@gmail.com>
2024-12-24 09:34:44 -0600

    ♻️ refactoring catch error handling to be manage from global exception filter

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,7 +1,6 @@
 import {
   BadRequestException,
   Injectable,
-  InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
 import { Category, Prisma } from '@prisma/client';
@@ -12,19 +11,11 @@ export class CategoriesService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async createCategory(data: Prisma.CategoryCreateInput): Promise<Category> {
-    try {
-      const category = await this.findCategoryByName(data.category_name);
-      if (category) {
-        throw new BadRequestException('Category already exists');
-      }
-      return this.prismaService.category.create({ data });
-    } catch (error) {
-      if (error instanceof BadRequestException) {
-        throw new BadRequestException(error.message);
-      }
-      console.error('Error creating category:', error);
-      throw new InternalServerErrorException('Failed to create category');
+    const existingCategory = await this.findCategoryByName(data.category_name);
+    if (existingCategory) {
+      throw new BadRequestException('Category already exists');
     }
+    return this.prismaService.category.create({ data });
   }
 
   async removeCategory(id: number): Promise<Category> {
@@ -36,44 +27,25 @@ export class CategoriesService {
       throw new NotFoundException(`Category with id ${id} not found`);
     }
 
-    try {
-      return await this.prismaService.category.delete({ where: { id } });
-    } catch (error) {
-      console.error(`Error deleting category with id ${id}:`, error);
-
-      if (error instanceof NotFoundException) {
-        throw error;
-      }
-
-      throw new InternalServerErrorException('Failed to delete category');
-    }
+    return this.prismaService.category.delete({ where: { id } });
   }
 
   async getAllCategories(): Promise<Category[]> {
-    try {
-      const categories: Category[] =
-        await this.prismaService.category.findMany();
-      if (categories.length === 0) {
-        throw new NotFoundException(`Categories not found`);
-      }
-      return this.prismaService.category.findMany();
-    } catch (error) {
-      console.error('Error finding all categories:', error);
-      if (error instanceof NotFoundException) {
-        throw error;
-      }
-      throw new InternalServerErrorException('Failed to find all categories');
+    const categories = await this.prismaService.category.findMany();
+    if (categories.length === 0) {
+      throw new NotFoundException('No categories found');
     }
+    return categories;
   }
 
-  async findCategoryByName(name: string): Promise<Category | null> {
-    try {
-      return this.prismaService.category.findFirst({
-        where: { category_name: name },
-      });
-    } catch (error) {
-      console.error(`Error finding category with name ${error}`);
-      throw new InternalServerErrorException('Failed to find category by name');
+  async findCategoryByName(name: string): Promise<Category> {
+    const category = await this.prismaService.category.findFirst({
+      where: { category_name: name },
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Category with name ${name} not found`);
     }
+    return category;
   }
 }
