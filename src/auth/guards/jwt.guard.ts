029ef15ffa93e1e4ac47dee73406c90be919134a
marvnramos <mr487223@gmail.com>
2024-12-23 19:22:17 -0600

    ♻️ refactor to use just one JWT authentication guard for both, http and graphql requests

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,5 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext): Promise<any> {
+    const contextType = context.getType<string>();
+
+    switch (contextType) {
+      case 'http':
+        return context.switchToHttp().getRequest();
+      case 'graphql':
+        const gqlContext = GqlExecutionContext.create(context);
+        return gqlContext.getContext().request;
+      default:
+        throw new UnauthorizedException(
+          `Unsupported context type: ${contextType}`,
+        );
+    }
+  }
+}
