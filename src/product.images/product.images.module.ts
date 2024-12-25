d5018d8d508508fe0e11dd985c44d765bee5804f
marvnramos <mr487223@gmail.com>
2024-12-24 18:08:44 -0600

    🔥 remove product images server module

--- src/product.images/product.images.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductImagesService } from './product.images.service';
-
-@Module({
-  providers: [ProductImagesService],
-})
-export class ProductImagesModule {}
