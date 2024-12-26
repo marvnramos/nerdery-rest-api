f49e09881803e9d59bbff181400788abe4dcdede
marvnramos <mr487223@gmail.com>
2024-12-25 20:48:15 -0600

    ✨ add updateProduct resolver

--- src/products/products.resolver.ts
+++ src/products/products.resolver.ts
@@ -1,27 +1,34 @@
 import { UseFilters } from '@nestjs/common';
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
-import { Product } from './models/products.model';
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { AddProductReq } from './dto/requests/create.product.req';
 import { GlobalExceptionFilter } from '../utils/GlobalExceptionFilter';
 import { ProductsService } from './products.service';
 import { plainToInstance } from 'class-transformer';
 import { Auth } from 'src/auth/decorators/auth.role.decorator';
+import { AddProductRes } from './dto/responses/create.product.res';
+import { UpdateProductRes } from './dto/responses/update.product.images.req';
+import { UpdateProductReq } from './dto/requests/update.product.req';
 
 @Resolver()
 export class ProductsResolver {
   constructor(private readonly productService: ProductsService) {}
 
   @Auth('MANAGER')
-  @Mutation(() => Product)
+  @Mutation(() => AddProductRes)
   @UseFilters(new GlobalExceptionFilter())
-  async addProduct(@Args('data') data: AddProductReq): Promise<Product> {
+  async addProduct(@Args('data') data: AddProductReq): Promise<AddProductRes> {
     const product = await this.productService.createProduct(data);
-    console.log('done ✅');
-    return plainToInstance(Product, product);
+    return plainToInstance(AddProductRes, product);
   }
 
-  @Query(() => String)
-  publicResource(): string {
-    return 'Public resource';
+  @Auth('MANAGER')
+  @Mutation(() => UpdateProductRes)
+  @UseFilters(new GlobalExceptionFilter())
+  async updateProduct(
+    @Args('id') id: string,
+    @Args('data') data: UpdateProductReq,
+  ): Promise<UpdateProductRes> {
+    const product = await this.productService.editProductData(id, data);
+    return plainToInstance(UpdateProductRes, product);
   }
 }
