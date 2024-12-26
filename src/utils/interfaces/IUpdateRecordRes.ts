7ed2461a4e23a867992ebd09b1f2dfe82dcf6b59
marvnramos <mr487223@gmail.com>
2024-12-25 20:44:41 -0600

    ✨ add update record response

--- src/utils/interfaces/IUpdateRecordRes.ts
+++ src/utils/interfaces/IUpdateRecordRes.ts
@@ -1,7 +1,9 @@
 import { Field, InterfaceType } from '@nestjs/graphql';
+import { Expose } from 'class-transformer';
 
 @InterfaceType()
 export abstract class IUpdateRecordRes {
   @Field(() => Date!, { name: 'updated_at' })
+  @Expose({ name: 'updated_at' })
   updatedAt: Date;
 }
