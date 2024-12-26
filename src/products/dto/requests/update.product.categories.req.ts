7899a0d076817d45c0efc7419b5b8cd56070951c
marvnramos <mr487223@gmail.com>
2024-12-26 00:48:22 -0600

    ✨ add update product categories graphql request

---
+++ src/products/dto/requests/update.product.categories.req.ts
@@ -0,0 +1,20 @@
+import { Field, InputType, ID, Int } from '@nestjs/graphql';
+import { IsArray, IsEnum, IsNotEmpty, IsString, IsInt } from 'class-validator';
+import { OperationType } from '../../../utils/enums/operation.enum';
+
+@InputType()
+export class UpdateProductCategoriesReq {
+  @Field(() => ID!)
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+
+  @Field(() => String!)
+  @IsEnum(OperationType)
+  op: OperationType;
+
+  @Field(() => [Int!]!)
+  @IsArray()
+  @IsInt({ each: true })
+  categories: number[];
+}
