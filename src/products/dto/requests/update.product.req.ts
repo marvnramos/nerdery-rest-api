2473a7af175d05a0b2bd929ec3f7df0bbd6baac5
marvnramos <mr487223@gmail.com>
2024-12-25 20:46:37 -0600

    ✨ add update product graphql input

---
+++ src/products/dto/requests/update.product.req.ts
@@ -0,0 +1,41 @@
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { Expose } from 'class-transformer';
+import {
+  IsBoolean,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+
+@InputType()
+export class UpdateProductReq {
+  @Field(() => String, { nullable: true, name: 'product_name' })
+  @Expose({ name: 'product_name' })
+  @IsOptional()
+  @IsString()
+  productName?: string;
+
+  @Field(() => String, { nullable: true })
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @Field(() => Int, { nullable: true })
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  stock?: number;
+
+  @Field(() => Boolean, { nullable: true, name: 'is_available' })
+  @Expose({ name: 'is_available' })
+  @IsOptional()
+  @IsBoolean()
+  isAvailable?: boolean;
+
+  @Field(() => Int, { nullable: true, name: 'unit_price' })
+  @Expose({ name: 'unit_price' })
+  @IsOptional()
+  @IsNumber()
+  unitPrice?: number;
+}
