17873489c6c387a15a42dece776ff2cac7c67e68
marvnramos <marvinramos@ravn.co>
2024-12-27 12:28:16 -0600

    🚚 method renamed

--- src/carts/carts.resolver.ts
+++ src/carts/carts.resolver.ts
@@ -3,7 +3,7 @@ import { CartsService } from './carts.service';
 import { Auth } from 'src/auth/decorators/auth.role.decorator';
 import { UseFilters } from '@nestjs/common';
 import { GlobalExceptionFilter } from '../utils/GlobalExceptionFilter';
-import { AddProductCartArg } from './dto/args/add.product.cart.arg';
+import { AddOrUpdateProductCartArgs } from './dto/args/add.or.update.product.cart.args';
 import { UpdateProductCartRes } from './dto/response/update.product.cart.res';
 
 @Resolver()
@@ -13,10 +13,14 @@ export class CartsResolver {
   @Auth('CLIENT')
   @Mutation(() => UpdateProductCartRes)
   @UseFilters(new GlobalExceptionFilter())
-  async addProductToCart(
-    @Args('data') addProductCartArg: AddProductCartArg,
+  async addOrUpdateCartProduct(
+    @Args('data')
+    addOrRemoveProductQuantityCartArg: AddOrUpdateProductCartArgs,
     @Context('request') req: any,
   ): Promise<UpdateProductCartRes> {
-    return this.cartsService.addProductToCart(req.user.id, addProductCartArg);
+    return this.cartsService.addProductToCart(
+      req.user.id,
+      addOrRemoveProductQuantityCartArg,
+    );
   }
 }
