3495d4fa3838dd8c976e97baaade1ffc49ace302
marvnramos <marvinramos@ravn.co>
2024-12-27 10:57:39 -0600

    ✨ addProductToCart resolver

--- src/carts/carts.resolver.ts
+++ src/carts/carts.resolver.ts
@@ -1,4 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { Mutation, Resolver, Args, Context } from '@nestjs/graphql';
+import { CartsService } from './carts.service';
+import { Auth } from 'src/auth/decorators/auth.role.decorator';
+import { UseFilters } from '@nestjs/common';
+import { GlobalExceptionFilter } from '../utils/GlobalExceptionFilter';
+import { AddProductCartArg } from './dto/args/add.product.cart.arg';
+import { UpdateProductCartRes } from './dto/response/update.product.cart.res';
 
 @Resolver()
-export class CartsResolver {}
+export class CartsResolver {
+  constructor(private readonly cartsService: CartsService) {}
+
+  @Auth('CLIENT')
+  @Mutation(() => UpdateProductCartRes)
+  @UseFilters(new GlobalExceptionFilter())
+  async addProductToCart(
+    @Args('data') addProductCartArg: AddProductCartArg,
+    @Context('request') req: any,
+  ): Promise<UpdateProductCartRes> {
+    return this.cartsService.addProductToCart(req.user.id, addProductCartArg);
+  }
+}
