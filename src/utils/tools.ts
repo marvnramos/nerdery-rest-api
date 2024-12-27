718d6fbbdce45397950cbb9e5c71745713683191
marvnramos <marvinramos@ravn.co>
2024-12-26 19:59:33 -0600

    ✨ add encode and decode functions

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -9,3 +9,11 @@ export function filterNullEntries(obj: object) {
     ),
   );
 }
+
+export function encodeBase64(data: string) {
+  return Buffer.from(data).toString('base64');
+}
+
+export function decodeBase64(data: string) {
+  return Buffer.from(data, 'base64').toString('utf-8');
+}
