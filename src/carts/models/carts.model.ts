d1126c41f57b19974471cf6bf6b7d6273bd071c0
marvnramos <marvinramos@ravn.co>
2024-12-27 19:02:35 -0600

    ✨ add cart items field

--- src/carts/models/carts.model.ts
+++ src/carts/models/carts.model.ts
@@ -1,12 +1,13 @@
 import { Field, ObjectType, ID } from '@nestjs/graphql';
+import { CartItem } from './cart.items.model';
 
 @ObjectType()
 export class Cart {
   @Field(() => ID)
   id: string;
 
-  @Field({ name: 'user_id' })
-  userId: string;
+  @Field(() => [CartItem!]!)
+  cartItems: CartItem[];
 
   @Field({ name: 'created_at' })
   createdAt: Date;
