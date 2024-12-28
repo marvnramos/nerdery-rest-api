4ed247eb601a51f6f6ce3361c2f32aad449ba880
marvnramos <mr487223@gmail.com>
2024-12-27 22:58:17 -0600

    ✨ add favorites services

--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../utils/prisma/prisma.service';
+import { plainToInstance } from 'class-transformer';
+import { AddFavoriteRes } from './dto/responses/add.favorite.res';
+import { Favorite } from '@prisma/client';
+import { RemoveFavoriteRes } from './dto/responses/remove.favorite.res';
 
 @Injectable()
-export class FavoritesService {}
+export class FavoritesService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async checkOrUncheckAsFavorite(
+    userId: string,
+    productId: string,
+  ): Promise<AddFavoriteRes | RemoveFavoriteRes> {
+    const existingFavorite = await this.prismaService.favorite.findUnique({
+      where: {
+        user_id_product_id: {
+          user_id: userId,
+          product_id: productId,
+        },
+      },
+    });
+
+    if (existingFavorite) {
+      await this.removeFavorite(existingFavorite.id);
+      return this.createRemoveFavoriteResponse();
+    }
+
+    const newFavorite = await this.addFavorite(userId, productId);
+    return plainToInstance(AddFavoriteRes, newFavorite);
+  }
+
+  private async removeFavorite(favoriteId: string): Promise<void> {
+    await this.prismaService.favorite.delete({
+      where: { id: favoriteId },
+    });
+  }
+
+  private createRemoveFavoriteResponse(): RemoveFavoriteRes {
+    const response = new RemoveFavoriteRes();
+    response.deletedAt = new Date();
+    return response;
+  }
+
+  private async addFavorite(
+    userId: string,
+    productId: string,
+  ): Promise<Favorite> {
+    return this.prismaService.favorite.create({
+      data: {
+        user: { connect: { id: userId } },
+        product: { connect: { id: productId } },
+      },
+    });
+  }
+}
