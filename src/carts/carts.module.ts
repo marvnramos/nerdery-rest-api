2ba43fb76a39e1061723411e2367fcf642639211
marvnramos <mr487223@gmail.com>
2024-12-27 21:53:46 -0600

    🔥remove product module

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { CartsResolver } from './carts.resolver';
 import { CartsService } from './carts.service';
 import { PrismaModule } from 'src/utils/prisma/prisma.module';
-import { ProductsModule } from 'src/products/products.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule],
   providers: [CartsResolver, CartsService],
   exports: [CartsService],
 })
