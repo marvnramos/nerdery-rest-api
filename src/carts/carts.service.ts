8bd72d05bc7c8954257ff9b9405a634eff71cd59
marvnramos <mr487223@gmail.com>
2024-12-27 20:38:32 -0600

    ✨ add service to get cart's items by cart id

--- src/carts/carts.service.ts
+++ src/carts/carts.service.ts
@@ -8,9 +8,11 @@ import { AddOrUpdateProductCartArgs } from './dto/args/add.or.update.product.car
 import { UpdateProductCartRes } from './dto/response/update.product.cart.res';
 import { ProductsService } from 'src/products/products.service';
 import { Cart, CartItem } from '@prisma/client';
-import { plainToInstance } from 'class-transformer';
+import { plainToClass, plainToInstance } from 'class-transformer';
 import { RemoveProductFromCartArgs } from './dto/args/remove.product.from.cart.args';
 import { RemoveProductFromCartRes } from './dto/response/remove.product.from.cart.res';
+import { Cart as CartType } from './models/carts.model';
+import { CartItem as CartItemType } from './models/cart.items.model';
 
 @Injectable()
 export class CartsService {
@@ -117,8 +119,8 @@ export class CartsService {
     return cart;
   }
 
-  async getCartDetails(userId: string) {
-    return this.prismaService.cart.findUnique({
+  async getCartByUserId(userId: string): Promise<CartType> {
+    const cart = await this.prismaService.cart.findUnique({
       where: { user_id: userId },
       include: {
         cart_items: {
@@ -134,20 +136,67 @@ export class CartsService {
       },
     });
 
-    // const productIds = await this.prismaService.cartItem.findMany({
-    //   where: { cart: { user_id: userId } },
-    //   select: { product_id: true },
-    // });
-    // const filterIds = productIds.map((item) => item.product_id);
-    // const products =
-    //   await this.productService.getProductsFilteredByIds(filterIds);
-    //
-    // console.log(products);
+    return plainToClass(CartType, {
+      ...cart,
+      createdAt: cart.created_at,
+      updatedAt: cart.updated_at,
+      cartItems: cart.cart_items.map((item) => ({
+        ...item,
+        product: item.product,
+      })),
+    });
   }
 
-  async getCartItems(cartId: string): Promise<CartItem[]> {
-    return this.prismaService.cartItem.findMany({
+  async getCartItemsByCartId(cartId: string): Promise<CartItemType[]> {
+    const cartItems = await this.prismaService.cartItem.findMany({
       where: { cart_id: cartId },
+      include: {
+        product: {
+          include: {
+            categories: {
+              select: {
+                category: { select: { id: true, category_name: true } },
+              },
+            },
+            images: {
+              select: {
+                id: true,
+                image_url: true,
+                public_id: true,
+              },
+            },
+          },
+        },
+      },
     });
+
+    return plainToInstance(
+      CartItemType,
+      cartItems.map((item) => ({
+        id: item.id,
+        quantity: item.quantity,
+        created_at: item.created_at,
+        updated_at: item.updated_at,
+        product: {
+          id: item.product.id,
+          productName: item.product.product_name,
+          description: item.product.description,
+          stock: item.product.stock,
+          isAvailable: item.product.is_available,
+          unitPrice: item.product.unit_price,
+          categories: item.product.categories.map((relation) => ({
+            id: relation.category.id,
+            categoryName: relation.category.category_name,
+          })),
+          images: item.product.images.map((image) => ({
+            id: image.id,
+            image_url: image.image_url,
+            public_id: image.public_id,
+          })),
+          createdAt: item.product.created_at,
+          updatedAt: item.product.updated_at,
+        },
+      })),
+    );
   }
 }
