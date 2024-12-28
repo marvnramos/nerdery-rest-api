e7d526ebb0363b3d6f01cd749139587a9d79551d
marvnramos <mr487223@gmail.com>
2024-12-28 11:14:07 -0600

    🔥removed products module

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { FavoritesResolver } from './favorites.resolver';
 import { FavoritesService } from './favorites.service';
 import { PrismaModule } from '../utils/prisma/prisma.module';
-import { ProductsModule } from '../products/products.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule],
   providers: [FavoritesResolver, FavoritesService],
   exports: [FavoritesResolver, FavoritesService],
 })
