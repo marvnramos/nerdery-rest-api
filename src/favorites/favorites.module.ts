bad8df45c94c1cbbf69dcebd597866e83950d8c3
marvnramos <mr487223@gmail.com>
2024-12-28 11:05:04 -0600

    ✨ importing products module

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FavoritesResolver } from './favorites.resolver';
 import { FavoritesService } from './favorites.service';
 import { PrismaModule } from '../utils/prisma/prisma.module';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProductsModule],
   providers: [FavoritesResolver, FavoritesService],
   exports: [FavoritesResolver, FavoritesService],
 })
