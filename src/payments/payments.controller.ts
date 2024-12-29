43b031bc72c7a0bcf7e23a103bba59d6b1fcc0e2
marvnramos <mr487223@gmail.com>
2024-12-29 17:56:14 -0600

    ✨ add create payment end point

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseFilters } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { GlobalExceptionFilter } from '../utils/GlobalExceptionFilter';
+import { AddPaymentReq } from './dto/requests/add.payment.req';
+import { AddPaymentRes } from './dto/responses/add.payment.res';
 
+@UseFilters(GlobalExceptionFilter)
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentService: PaymentsService) {}
 
   @Post()
-  async createPayment(
-    @Body('orderId') orderId: string,
-    @Body('amount') amount: number,
-  ) {
-    return this.paymentService.createPaymentIntent(orderId, amount);
+  async createPayment(@Body() req: AddPaymentReq): Promise<AddPaymentRes> {
+    return this.paymentService.createPaymentIntent(req.orderId);
   }
 
   @Post('webhook')
