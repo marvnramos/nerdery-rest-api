0bbd13ead5650363ca01d6f4a078f47a1974c11d
marvnramos <mr487223@gmail.com>
2024-12-29 17:56:32 -0600

    ✨ importing orders module

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -3,9 +3,10 @@ import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../utils/prisma/prisma.module';
+import { OrdersModule } from 'src/orders/orders.module';
 
 @Module({
-  imports: [ConfigModule, PrismaModule],
+  imports: [ConfigModule, PrismaModule, OrdersModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
