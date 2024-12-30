5b89f643760e13dbcea0fe6492d3e4a2ff1a3a3e
marvnramos <mr487223@gmail.com>
2024-12-29 23:32:46 -0600

    🚚 renamed file

--- src/orders/dto/responses/orders.pagination.type.res.ts
+++ src/orders/dto/responses/orders.pagination.type.res.ts
@@ -1,6 +1,6 @@
 import { ObjectType } from '@nestjs/graphql';
 import PaginationType from 'src/utils/pagination/pagination';
-import { OrderType } from './order.type';
+import { OrderType } from '../../types/order.type';
 
 @ObjectType()
 export class PaginatedOrdersType extends PaginationType(OrderType) {}
