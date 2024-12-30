31fdddd7c1778209d269172605569c6aa437efb9
marvnramos <mr487223@gmail.com>
2024-12-29 19:38:08 -0600

    ✏️ using snake case convention for names

--- src/orders/types/order.type.ts
+++ src/orders/types/order.type.ts
@@ -1,39 +1,31 @@
 import { ObjectType, Field, ID } from '@nestjs/graphql';
 import { OrderDetailType } from './order.detail.type';
-import { Expose, Type } from 'class-transformer';
+import { User } from '../../users/models/users.model';
+import { Expose } from "class-transformer";
 
 @ObjectType()
 export class OrderType {
   @Field(() => ID)
-  @Expose()
   id: string;
 
-  @Field(() => ID, { name: 'user_id' })
-  @Expose({ name: 'user_id' })
-  @Expose()
-  userId: string;
+  @Field(() => String)
+  user_id: string;
 
-  @Field()
-  @Expose()
+  @Field(() => String)
   address: string;
 
-  @Field({ name: 'nearby_landmark' })
-  @Expose({ name: 'nearby_landmark' })
-  @Expose()
-  nearbyLandmark: string;
+  @Field(() => String)
+  nearby_landmark: string;
 
-  @Field({ name: 'created_at' })
-  @Expose({ name: 'created_at' })
-  @Expose()
-  createdAt: Date;
+  @Field(() => Date)
+  created_at: Date;
 
-  @Field({ name: 'updated_at' })
-  @Expose({ name: 'updated_at' })
-  @Expose()
-  updatedAt: Date;
+  @Field(() => Date)
+  updated_at: Date;
+
+  @Field(() => User, { nullable: true })
+  user?: User | null;
 
   @Field(() => [OrderDetailType!])
-  @Type(() => OrderDetailType)
-  @Expose()
-  orderDetails: OrderDetailType[];
+  order_details: OrderDetailType[];
 }
