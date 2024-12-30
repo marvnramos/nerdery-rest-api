6ae61e882d470a9729aef1dee1616d11267058ab
marvnramos <mr487223@gmail.com>
2024-12-30 00:03:55 -0600

    ✏️ removed expose

--- src/categories/dto/responses/delete.category.res.ts
+++ src/categories/dto/responses/delete.category.res.ts
@@ -1,9 +1,7 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { Expose } from 'class-transformer';
 
 @ObjectType()
 export class DeleteCategoryRes {
   @Field(() => Date, { name: 'deleted_at' })
-  @Expose({ name: 'deleted_at' })
   deletedAt: Date;
 }
