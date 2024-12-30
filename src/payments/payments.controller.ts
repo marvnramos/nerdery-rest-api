61adc1ab087c29fbb90ae21f7e69de12da4c7689
marvnramos <mr487223@gmail.com>
2024-12-29 22:02:00 -0600

    ✨ get user info to be pass by params

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Post, Body, UseFilters } from '@nestjs/common';
+import { Controller, Post, Body, UseFilters, Request } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { GlobalExceptionFilter } from '../utils/GlobalExceptionFilter';
 import { AddPaymentReq } from './dto/requests/add.payment.req';
-import { AddPaymentRes } from './dto/responses/add.payment.res';
+import { Auth } from '../auth/decorators/auth.role.decorator';
 
 @UseFilters(GlobalExceptionFilter)
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentService: PaymentsService) {}
 
+  @Auth('CLIENT')
   @Post()
-  async createPayment(@Body() req: AddPaymentReq): Promise<AddPaymentRes> {
-    return this.paymentService.createPaymentIntent(req.orderId);
+  async createPayment(
+    @Body() req: AddPaymentReq,
+    @Request() { user }: any,
+  ): Promise<void> {
+    await this.paymentService.createPaymentIntent(req.orderId, user);
   }
 
   @Post('webhook')
