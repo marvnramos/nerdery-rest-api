63a919b9bf12c23e92a8a030b382d66ae6e4dfe6
marvnramos <mr487223@gmail.com>
2024-12-29 19:40:05 -0600

    ✏️ using snake case convention for names

--- src/utils/pagination/pageinfo.type.ts
+++ src/utils/pagination/pageinfo.type.ts
@@ -2,11 +2,11 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
 export class PageInfoType {
-  @Field(() => String!, { name: 'start_cursor' })
-  startCursor: string;
+  @Field(() => String, { name: 'start_cursor', nullable: true })
+  startCursor?: string;
 
-  @Field(() => String!, { name: 'end_cursor' })
-  endCursor: string;
+  @Field(() => String, { name: 'end_cursor', nullable: true })
+  endCursor?: string;
 
   @Field(() => Boolean!, { name: 'has_next_page' })
   hasNextPage: boolean;
