e4928192b263109b22c8ab8ea678e6ef6f980868
marvnramos <mr487223@gmail.com>
2024-12-30 19:58:10 -0600

    ✨ exposing name with snake case

--- src/carts/types/cart.type.ts
+++ src/carts/types/cart.type.ts
@@ -6,8 +6,8 @@ export class CartType {
   @Field(() => ID)
   id: string;
 
-  @Field(() => [CartItemType!]!)
-  cart_item: CartItemType[];
+  @Field(() => [CartItemType!]!, { name: 'cart_item' })
+  cart_items: CartItemType[];
 
   @Field()
   created_at: Date;
