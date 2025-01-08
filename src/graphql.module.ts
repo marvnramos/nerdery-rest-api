f9f16affca1bca48da2662f3299ea317f91648e1
marvnramos <mr487223@gmail.com>
2025-01-07 23:05:09 -0600

    ✨ add data loaders to graphql context

--- src/graphql.module.ts
+++ src/graphql.module.ts
@@ -4,19 +4,29 @@ import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { EnvsConfigModule } from './config/envs.config.module';
 import { EnvsConfigService } from './config/envs.config.service';
+import { DataLoaderModule } from './data.loader/data.loader.module';
+import { DataLoaderService } from './data.loader/data.loader.service';
 
 @Module({
   imports: [
     EnvsConfigModule,
+    DataLoaderModule,
     GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      imports: [EnvsConfigModule],
-      inject: [EnvsConfigService],
-      useFactory: async (envsConfigService: EnvsConfigService) => ({
+      imports: [EnvsConfigModule, DataLoaderModule],
+      inject: [EnvsConfigService, DataLoaderService],
+      useFactory: async (
+        envsConfigService: EnvsConfigService,
+        dataloaderService: DataLoaderService,
+      ) => ({
         autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
         playground: envsConfigService.getNodeEnv() !== 'production',
         debug: true,
-        context: ({ req, res }) => ({ request: req, response: res }),
+        context: ({ req, res }) => ({
+          request: req,
+          response: res,
+          loaders: dataloaderService.getLoaders(),
+        }),
         formatError: (error) => {
           const { message, extensions } = error;
 
