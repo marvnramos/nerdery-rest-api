9c0265d4c4700a892627409a944aafa1c115bb16
marvnramos <marvinramos@ravn.co>
2025-01-09 14:16:32 -0600

    🚚 update path import

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -2,7 +2,7 @@ import { Args, Context, Mutation, Query, ResolveField, Resolver } from '@nestjs/
 import { OrdersService } from './orders.service';
 import { Auth } from '../auth/decorators/auth.role.decorator';
 import { UseFilters } from '@nestjs/common';
-import { GlobalExceptionFilter } from '../utils/exception/GlobalExceptionFilter';
+import { GlobalExceptionFilter } from '../../utils/exception/GlobalExceptionFilter';
 import { AddOrderRes } from './dto/responses/add.order.res';
 import { AddOrderArgs } from './dto/args/add.order.args';
 import { GetOrdersArgs } from './dto/args/get.orders.args';
