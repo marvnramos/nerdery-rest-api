bd77f3084703f3b1f2806dcaef418d86f02d327d
marvnramos <marvinramos@ravn.co>
2025-01-10 07:01:27 -0600

    ✨ imported envs module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,15 @@ import { UsersService } from './users.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { MailModule } from '../mailer/mail.module';
 import { VerificationTokenModule } from '../verification.token/verification.token.module';
+import { EnvsConfigModule } from '../../utils/config/envs.config.module';
 
 @Module({
-  imports: [PrismaModule, MailModule, VerificationTokenModule],
+  imports: [
+    PrismaModule,
+    MailModule,
+    VerificationTokenModule,
+    EnvsConfigModule,
+  ],
   providers: [UsersService],
   exports: [UsersService],
 })
