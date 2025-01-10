4cda9800cc7f9ba932234f3d71bbee6c2517ac56
marvnramos <marvinramos@ravn.co>
2025-01-10 07:01:56 -0600

    ♻️ moved logic to services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,4 @@
 import {
-  BadRequestException,
   Body,
   Controller,
   Get,
@@ -14,79 +13,26 @@ import {
 import { UsersService } from './users.service';
 import { SignupReqDto } from './dto/requests/signup.req.dto';
 import { SignUpResDto } from './dto/responses/signup.res.dto';
-import { randomUUID } from 'crypto';
-import { MailService } from '../mailer/mail.service';
-import { getExpirationTimestamp } from '../../utils/index.util';
 import { ForgotPasswordReqDto } from './dto/requests/forgot.password.req.dto';
 import { ResetPasswordResDto } from './dto/responses/reset.password.res.dto';
 import { Response } from 'express';
 import { ResetPasswordReqDto } from './dto/requests/reset.password.req.dto';
-import { VerificationTokenService } from '../verification.token/verification.token.service';
 import { seconds, Throttle } from '@nestjs/throttler';
-import { EnvsConfigService } from '../../utils/config/envs.config.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(
-    private readonly usersService: UsersService,
-    private readonly verificationTokenService: VerificationTokenService,
-    private readonly mailService: MailService,
-    private readonly envsConfigService: EnvsConfigService,
-  ) {}
-
-  private baseUrl = this.envsConfigService.getBaseUrl();
+  constructor(private readonly usersService: UsersService) {}
 
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
   async signUp(@Body() req: SignupReqDto): Promise<SignUpResDto> {
-    const existingUser = await this.usersService.findByEmail(req.email);
-    if (existingUser) {
-      throw new BadRequestException('Email already exists');
-    }
-
-    req.password = await this.usersService.hashPassword(req.password);
-    const userData = { ...req, is_email_verified: false };
-
-    const userPromise = this.usersService.create(userData);
-    const token = randomUUID();
-    const tokenData = {
-      token,
-      is_used: false,
-      expired_at: getExpirationTimestamp(),
-      user: { connect: { id: (await userPromise).id } },
-      tokenType: { connect: { id: 1 } },
-    };
-
-    const [user, , encodedToken] = await Promise.all([
-      userPromise,
-      this.verificationTokenService.create(tokenData),
-      this.verificationTokenService.encodeVerificationToken(token),
-    ]);
-
-    await this.mailService.sendEmail({
-      email: user.email,
-      fullName: `${user.first_name} ${user.last_name}`,
-      subject: 'Email Verification',
-      uri: `${this.baseUrl}/users/validate-email/${encodedToken}`,
-      template: './confirmation',
-    });
-
-    return {
-      created_at: user.created_at,
-    };
+    return this.usersService.signUp(req);
   }
 
   @Get('validate-email/:token')
   @HttpCode(HttpStatus.NO_CONTENT)
   async validateEmail(@Param('token') token: string): Promise<void> {
-    const decodedToken =
-      await this.verificationTokenService.decodeVerificationToken(token);
-    const verifiedEmail = await this.usersService.verifyEmail(decodedToken);
-    if (!verifiedEmail) {
-      throw new BadRequestException(
-        'This link has expired or is already used.',
-      );
-    }
+    return this.usersService.validateEmail(token);
   }
 
   @Throttle({ default: { ttl: seconds(60), limit: 1 } })
@@ -95,73 +41,20 @@ export class UsersController {
   async forgotPassword(
     @Body() req: ForgotPasswordReqDto,
   ): Promise<ResetPasswordResDto> {
-    const user = await this.usersService.findByEmail(req.email);
-    if (!user) {
-      throw new BadRequestException('Email not found');
-    }
-
-    const token = randomUUID();
-    const tokenData = {
-      token,
-      is_used: false,
-      expired_at: getExpirationTimestamp(),
-      user: { connect: { id: user.id } },
-      tokenType: { connect: { id: 2 } },
-    };
-
-    const encodedToken =
-      await this.verificationTokenService.encodeVerificationToken(token);
-    await Promise.all([
-      this.verificationTokenService.create(tokenData),
-      this.mailService.sendEmail({
-        email: user.email,
-        subject: 'Reset Password',
-        uri: `${this.baseUrl}/users/reset-password/${encodedToken}`,
-        template: './reset-password',
-        fullName: `${user.first_name} ${user.last_name}`,
-      }),
-    ]);
-
-    return { message: 'Email sent' };
+    return this.usersService.forgotPassword(req);
   }
 
   @Throttle({ default: { ttl: seconds(60), limit: 3 } })
   @Get('reset-password/:token')
   @Render('reset-password-view')
   resetPasswordView(@Param('token') token: string, @Res() res: Response) {
-    const nonce = res.locals.nonce;
-    res.cookie('token', token, { httpOnly: false, maxAge: 900_000 });
-    return { nonce };
+    return this.usersService.resetPasswordView(token, res);
   }
 
   @Throttle({ default: { ttl: seconds(60), limit: 3 } })
   @Put('reset-password')
   @HttpCode(HttpStatus.NO_CONTENT)
   async resetPassword(@Body() req: ResetPasswordReqDto) {
-    const [decodedToken, hashedPassword] = await Promise.all([
-      this.verificationTokenService.decodeVerificationToken(req.token),
-      this.usersService.hashPassword(req.new_password),
-    ]);
-
-    const isPasswordReset = await this.usersService.resetPassword(
-      decodedToken,
-      hashedPassword,
-    );
-
-    const token =
-      await this.verificationTokenService.findVerificationToken(decodedToken);
-    const user = await this.usersService.findById(token.user_id);
-    await this.mailService.sendEmail({
-      email: user.email,
-      fullName: `${user.first_name} ${user.last_name}`,
-      subject: 'Password Reset',
-      template: './reset-password-confirmation',
-    });
-
-    if (!isPasswordReset) {
-      throw new BadRequestException(
-        'This link has expired or is already used.',
-      );
-    }
+    return this.usersService.resetPassword(req);
   }
 }
