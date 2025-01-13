14039fd3a37655dc3a59bb11dcdc9d376311d23c
marvnramos <marvinramos@ravn.co>
2025-01-13 08:23:20 -0600

    ✅ add new mocks

--- test/mocks/user.mocks.ts
+++ test/mocks/user.mocks.ts
@@ -1,3 +1,7 @@
+import { faker } from '@faker-js/faker/locale/ar';
+import { UserRoleType } from '@prisma/client';
+import { getExpirationTimestamp } from '../../utils/time.util';
+
 export class UserServiceMocks {
   static user = {
     first_name: 'John',
@@ -29,4 +33,35 @@ export class UserServiceMocks {
     id: 'user123',
     email: 'test@example.com',
   };
+
+  static createUserMock = {
+    id: faker.string.uuid(),
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.exampleEmail({ firstName: 'test' }),
+    address: faker.location.streetAddress(),
+    password: faker.internet.password(),
+    is_email_verified: true,
+    role_id: 1,
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
+
+  static userManagerRole = {
+    id: 1,
+    role: UserRoleType.MANAGER,
+  };
+
+  static uuid = faker.string.uuid();
+
+  static createToken = (userId: string, tokenTypeId?: number) => ({
+    id: faker.string.uuid(),
+    token: faker.string.uuid(),
+    user_id: userId,
+    token_type_id: tokenTypeId || 2,
+    is_used: false,
+    expired_at: getExpirationTimestamp(),
+    created_at: new Date(),
+    updated_at: new Date(),
+  });
 }
