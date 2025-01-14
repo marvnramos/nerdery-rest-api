eb8ae4e20d53bd78736d6c46db7a3ff1ab9c18ea
marvnramos <marvinramos@ravn.co>
2025-01-14 11:56:45 -0600

    ✅ add new cart mocks

--- test/mocks/cart.mocks.ts
+++ test/mocks/cart.mocks.ts
@@ -1,59 +1,68 @@
-import { Cart, CartItem } from '@prisma/client';
-import { UpdateProductCartRes } from '../../src/carts/dto/response/update.product.cart.res';
+import { faker } from '@faker-js/faker/locale/ar';
 
 export class CartServiceMocks {
-  static cart: Cart = {
-    id: '49c84175-021f-4603-a1b4-836f1f287dea',
-    user_id: 'c087cc65-32e2-405b-9c11-ccf413843724',
+  static createCart = (userId: string) => ({
+    id: faker.string.uuid(),
+    user_id: userId,
     created_at: new Date(),
     updated_at: new Date(),
-  };
+  });
 
-  static notAcceptableExceptionCart: Cart = {
-    id: 'cart123',
-    user_id: 'anotherUser',
+  static createCartItem = (
+    cartId: string,
+    productId: string,
+    quantity: number,
+  ) => ({
+    id: faker.string.uuid(),
+    cart_id: cartId,
+    product_id: productId,
+    quantity,
     created_at: new Date(),
     updated_at: new Date(),
-  };
+  });
 
-  static cartItemToGetCartById = [
-    {
-      id: 'item123',
-      cart_id: '49c84175-021f-4603-a1b4-836f1f287dea',
-      product_id: 'prod123',
-      quantity: 2,
-      created_at: new Date(),
-      updated_at: new Date(),
-      product: {
-        id: 'prod123',
-        product_name: 'Sample Product',
-        description: 'Test description',
-        stock: 10,
-        is_available: true,
-        unit_price: 100,
-        categories: [],
-        images: [],
+  static cartItemToGetCartById = () => {
+    const productId = faker.string.uuid();
+    return [
+      {
+        id: faker.string.uuid(),
+        cart_id: faker.string.uuid(),
+        product_id: productId,
+        quantity: 2,
         created_at: new Date(),
         updated_at: new Date(),
+        product: {
+          id: productId,
+          product_name: faker.commerce.product(),
+          description: faker.commerce.productDescription(),
+          stock: 10,
+          is_available: true,
+          unit_price: 100,
+          categories: [],
+          images: [],
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
       },
-    },
-  ];
+    ];
+  };
 
   static cartToGetCartByUserId = (userId: string) => {
+    const productId = faker.string.uuid();
     return {
-      id: 'cart123',
+      id: faker.string.uuid(),
       user_id: userId,
       cart_items: [
         {
-          id: 'item123',
-          product_id: 'prod123',
+          id: faker.string.uuid(),
+          product_id: productId,
           quantity: 2,
           created_at: new Date(),
           updated_at: new Date(),
           product: {
-            id: 'prod123',
-            product_name: 'Sample Product',
-            description: 'Test description',
+            id: productId,
+            product_name: faker.commerce.product(),
+            description: faker.commerce.productDescription(),
             stock: 10,
             is_available: true,
             unit_price: 100,
@@ -67,49 +76,5 @@ export class CartServiceMocks {
       created_at: new Date(),
       updated_at: new Date(),
     };
-  };
-
-  static cartFromDeleteProductFromCart: Cart = {
-    id: 'cart123',
-    user_id: 'user123',
-    created_at: new Date(),
-    updated_at: new Date(),
-  };
-
-  static cartItemToDelete: CartItem = {
-    id: 'item123',
-    created_at: new Date(),
-    updated_at: new Date(),
-    cart_id: 'cart123',
-    product_id: 'prod123',
-    quantity: 1,
-  };
-
-  static newUpsertCartItem = (
-    cartId: string,
-    productId: string,
-    quantity: number,
-  ) => {
-    return {
-      id: 'newCartItem456',
-      cart_id: cartId,
-      product_id: productId,
-      quantity,
-      created_at: new Date(),
-      updated_at: new Date(),
-    };
-  };
-
-  static cartItem: CartItem = {
-    id: '49c84175-021f-4603-a1b4-836f1f287dea',
-    quantity: 1,
-    cart_id: '49c84175-021f-4603-a1b4-836f1f287dea',
-    product_id: 'ff7aade2-3920-48ce-b419-8385b324f25c',
-    created_at: new Date(),
-    updated_at: new Date(),
-  };
-
-  static updatedCartItem: UpdateProductCartRes = {
-    updatedAt: new Date(),
   };
 }
